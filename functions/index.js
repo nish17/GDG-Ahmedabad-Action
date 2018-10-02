069ebde142cf14f06392d04ba629781b7df04bef
Nimesh Solanki <nimesh3536@gmail.com>
2018-10-02 16:44:45 +0530

    add AboutGDGIntent

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const {
   dialogflow,
   List,
   Suggestions,
+  LinkOutSuggestion,
   SimpleResponse
 } = require("actions-on-google");
 const app = dialogflow({ debug: true });
@@ -18,5 +19,26 @@ app.intent("New Welcome Intent", conv => {
   );
 });
 
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+app.intent("AboutGDGIntent", conv => {
+  conv.ask(
+    // `<speak> <sub alias="Google Developers Group">GDG</sub> <say-as interpret-as="characters">GDG</say-as> Ahmedabad is open and volunteer geek community who create exciting projects and share experience about Google technologies with a passion.</speak>`
+    `<speak>Google Developers Group Ahmedabad is open and volunteer geek community who create exciting projects and share experience about Google technologies with a passion.</speak>`
+  );
+  conv.ask(new Suggestions(`Past Events`));
+  conv.ask(
+    new LinkOutSuggestion(
+      {
+        name: `GDG Ahmedabad Website`,
+        url: `https://www.gdgahmedabad.com/`
+      }
+      // ,
+      // {
+      //   destinationName: "Visit DevFest Website",
+      //   url: "http://devfest.gdgahmedabad.com/"
+      // }
+    )
+  );
+});
+
+// https://firebase.google.com/docs/functions/write-firebase-functions
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
